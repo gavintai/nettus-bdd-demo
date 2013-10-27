8b0997036b5d5103c249a80cbe1d5674ace120dd
Gavin <gavin.tai@rapp.com.sg>
2013-10-27 10:49:00 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NettusBddDemo::Application.config.secret_token = 'f45dc6e6be0d13858efdb15c4e823f9696c20981b89f00af788023941fc8123f6dd0e7072d2f49aaac8004b1365ccc2ad6437d4892500ff774ccfc87afeddc2b'
